a3bb864f8fe978e7b782f7de32f4d6e7fc0256ea
Max Kokorin <kokorin.max@gmail.com>
2014-10-23 13:11:21 +0700

    Add doc

---
+++ doc.go
@@ -0,0 +1,10 @@
+// Copyright 2012 Max "theaidem" Kokorin. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+/*
+Package aerostore implements the Gorilla toolkit's sessions store for Aerospike datatabase.
+Gorilla's Sessions and their sessions store interface can be found [here](https://github.com/gorilla/sessions)
+*/
+
+package aerostore
